5bf82409289827f4d8b7882df505ba2135b8304e
spencerkershaw <spencerkershaw@gmail.com>
2013-11-18 10:12:12 -0500

    collision

--- American Hobo/American Hobo/americanHobo.cpp
+++ American Hobo/American Hobo/americanHobo.cpp
@@ -446,25 +446,9 @@ void AmericanHobo::collisions()
 	}
 
 	for(int i = 0; i < 10; i++) {
-		if(hobo[i].sword.collidesWith(hero,collisionVector)) {
-			hero.damage(SWORD, hobo[i].getVelocity());
-			/*
-			switch(hero.dir) {
-			case 0:
-				break;
-			case 1:
-				hero.damage(SWORD, D3DXVECTOR2(-1,0));
-				break;
-			case 2:
-				hero.damage(SWORD, D3DXVECTOR2(1,0));
-				break;
-			case 3:
-				hero.damage(SWORD, D3DXVECTOR2(0,1));
-				break;
-			case 4:
-				hero.damage(SWORD, D3DXVECTOR2(0,-1));
-				break;
-			}*/
+		if(brawler[i].sword.collidesWith(hero,collisionVector)) {
+			hero.damage(SWORD, brawler[i].getVelocity());
+
 		}
 	}
 
