9360d65c0faba0aaeaa20282976a41ef330b5776
Sariander <garrett.young17@gmail.com>
2013-12-11 10:59:47 -0500

    Thrower fix, level fix

--- American Hobo/American Hobo/americanHobo.cpp
+++ American Hobo/American Hobo/americanHobo.cpp
@@ -674,7 +674,7 @@ void AmericanHobo::collisions()
 
 	for(int i = 0; i < THROWER_NUMBER; i++) {
 		if(thrower[i].collidesWith(hero,collisionVector)) {
-			hero.damage(SWORD, thrower[i].getVelocity());
+			hero.damage(SWORD, -thrower[i].getVelocity());
 
 		}
 	}
